050dfb0b574cc278f550a9cb44ebf18cb8083492
Parth Tiwari <parthtiwari670@gmail.com>
2025-04-18 16:54:19 +0530

    Move AOS initialization and dark mode toggle function to Snake.html

--- index.js
+++
@@ -1,9 +0,0 @@
-src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"
-
-  AOS.init();
-
-  function toggleMode() {
-    document.body.classList.toggle('dark-mode');
-    const modeButton = document.getElementById('modeBtn');
-    modeButton.textContent = document.body.classList.contains('dark-mode') ? '🌞' : '🌙';
-  }
