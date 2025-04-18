85c8c682e25fd653cc02d8a1a9e35a9191bb0248
Parth Tiwari <parthtiwari670@gmail.com>
2025-04-18 16:53:52 +0530

    Refactor script loading and initialize AOS in index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-AOS.init();
+src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"
 
-    
-function toggleMode() {
-  document.body.classList.toggle('dark-mode');
-  const modeButton = document.getElementById('modeBtn');
-  modeButton.textContent = document.body.classList.contains('dark-mode') ? '🌞' : '🌙';
-}
+  AOS.init();
+
+  function toggleMode() {
+    document.body.classList.toggle('dark-mode');
+    const modeButton = document.getElementById('modeBtn');
+    modeButton.textContent = document.body.classList.contains('dark-mode') ? '🌞' : '🌙';
+  }
